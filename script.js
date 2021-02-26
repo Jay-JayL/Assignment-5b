f6ff83ea19c7a872d29ca1c8be77b936954b3f3e
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-02-26 22:36:23 +0000

    fixed more spacing

--- script.js
+++ script.js
@@ -15,5 +15,5 @@ function area () {
   number = 3.14 * 2 * r
   number2 = 3.14 * r ** 2
   document.getElementById('paragraph').innerHTML = number
- document.getElementById('paragraph2').innerHTML = number2
+  document.getElementById('paragraph2').innerHTML = number2
 }
