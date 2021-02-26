289e1ac4b5f3bdbaa829ed9f17aa606fa1061baa
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-02-26 22:21:13 +0000

    Fixed more errors in JS

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let input = 0
 document.getElementById('button').addEventListener('click', area)
 
 function area () {
-  d = document.getElementById('input').value
+  input = document.getElementById('input').value
 
-  d=parseInt(d)
+  input = parseInt(d)
 
-  r=d/2
+  r = input / 2
 
-  number = 3.14*2*r
+  number = 3.14 * 2 * r
 
-  number2 = 3.14*r**2
+  number2 = 3.14 * r ** 2
   document.getElementById('paragraph').innerHTML = number
  document.getElementById('paragraph2').innerHTML = number2
 }
