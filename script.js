ac5c70da330897e9ea113d3cc84f43a13e82e0ff
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-02-26 22:27:31 +0000

    fixed more errors

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let input = 0
+let r = 0
+let number = 0
+let number2 = 0
 
 document.getElementById('button').addEventListener('click', area)
 
@@ -10,7 +13,7 @@ function area () {
   r = input / 2
 
   number = 3.14 * 2 * r
-
+  
   number2 = 3.14 * r ** 2
   document.getElementById('paragraph').innerHTML = number
  document.getElementById('paragraph2').innerHTML = number2
