e112527ab2eeeb849487ce92b06e567a84190407
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-02-26 22:32:57 +0000

    fixed spacing

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function area () {
   r = input / 2
 
   number = 3.14 * 2 * r
-  
   number2 = 3.14 * r ** 2
   document.getElementById('paragraph').innerHTML = number
  document.getElementById('paragraph2').innerHTML = number2
